ec993b47990ac373d879dcd41e8d3bbcddf5875e
Vladimir <ecofovka@gmail.com>
2020-09-28 11:57:29 +0200

    Ex3 is done

--- week1/homework/exercises/3-web-server/server.js
+++ week1/homework/exercises/3-web-server/server.js
@@ -3,11 +3,27 @@
  */
 
 var http = require('http');
+const fs = require('fs');
 
 //create a server
 let server = http.createServer(function (req, res) {
   // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
+  if (req.url === '/') {
+	let data = fs.readFileSync('./index.html');
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write(data); // Sends a response back to the client
+  } else if (req.url === '/index.js') {
+	let data = fs.readFileSync('./index.js');
+	res.writeHead(200, {'Content-Type': 'application/json'});
+	res.write(data);
+  } else if (req.url === '/style.css') {
+	let data = fs.readFileSync('./style.css');
+	res.writeHead(200, {'Content-Type': 'text/css'});
+	res.write(data);
+  } else {
+	  res.writeHead(404, {'Content-Type': 'text/plain'});
+	  res.write('Error: Not Found');
+  }
 	res.end(); // Ends the response
 });
 
